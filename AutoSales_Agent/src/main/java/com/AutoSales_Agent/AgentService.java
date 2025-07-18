e7707f2c857ba7a9ec659544867d97fce4457eda
leppard204 <eyela1004@naver.com>
2025-07-19 02:05:33 +0900

    db연동

---
+++ AutoSales_Agent/src/main/java/com/AutoSales_Agent/AgentService.java
@@ -0,0 +1,11 @@
+package com.AutoSales_Agent;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service 
+public class AgentService {
+
+}
