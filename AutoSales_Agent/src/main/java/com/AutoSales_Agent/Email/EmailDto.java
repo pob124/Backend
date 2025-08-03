de624bfbaef84c40112249f9c56bb65faf4ea7ef
leppard204 <eyela1004@naver.com>
2025-08-03 09:40:05 +0900

    sendEmail 추가

--- AutoSales_Agent/src/main/java/com/AutoSales_Agent/Email/EmailDto.java
+++ AutoSales_Agent/src/main/java/com/AutoSales_Agent/Email/EmailDto.java
@@ -1,12 +1,20 @@
 package com.AutoSales_Agent.Email;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmailDto {
+	private static final long serialVersionUID = 1L;
 	
 	private Integer projectId;
 	private Integer leadId;
 	private String subject;
 	private String body;
+	private String contactEmail;
 }
