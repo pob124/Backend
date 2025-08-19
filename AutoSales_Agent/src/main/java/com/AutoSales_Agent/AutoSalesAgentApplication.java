c05c2bccd338c30bda219fd4c1ae8fd3d101ecb2
leppard204 <eyela1004@naver.com>
2025-08-19 16:07:50 +0900

    기본 설정

--- AutoSales_Agent/src/main/java/com/AutoSales_Agent/AutoSalesAgentApplication.java
+++ AutoSales_Agent/src/main/java/com/AutoSales_Agent/AutoSalesAgentApplication.java
@@ -2,10 +2,14 @@ package com.AutoSales_Agent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.AutoSales_Agent.Identity.AppIdentityProps;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableConfigurationProperties({ AppIdentityProps.class })
 public class AutoSalesAgentApplication {
 
 	public static void main(String[] args) {
